0765b7950c83dff53efc12a0174ab3237caefda7
fuuilstacker <mirohsnichenkopavlo802@gmail.com>
2024-11-09 09:35:57 +0200

    second comsmixt

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -148,7 +148,7 @@ class FileController {
 
             const user = await User.findById(req.user.id)
             const avatarName = Uuid.v4() + ".jpg"
-            file.mv(req.filePath + "\\" + avatarName)
+            file.mv(config.get('staticPath') + "\\" + avatarName)
             user.avatar = avatarName
             await user.save()
             return res.json(user)
@@ -161,7 +161,7 @@ class FileController {
     async deleteAvatar(req, res) {
         try {
             const user = await User.findById(req.user.id)
-            fs.unlinkSync(req.filePath + '\\' + user.avatar)
+            fs.unlinkSync(config.get('staticPath') + '\\' + user.avatar)
             user.avatar = null
             await user.save()
             return res.json(user)
